e8570f463401a58854fed85838d19c93375d22bf
Berke Alkaş <berkealkas19@hotmail.com>
2023-11-24 18:06:23 +0300

    commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section className="min-h-[50vh] flex items-center justify-center">
         <div className="container mx-auto mt-24">
-            <h1 className="text-white text-6xl text-center">Sağlık teknolojilerinde geleceğin çözümleri için MediTechLabs yanınızda</h1>
+            <h1 className="text-white text-6xl text-center leading-normal">Sağlık teknolojilerinde geleceğin çözümleri için MediTechLabs yanınızda</h1>
         </div>
     </section>
   )
